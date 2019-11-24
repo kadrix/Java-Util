fd6066cddaab5e0b67783d0bd8614e0a17d71323
Islam <kadrii@outlook.com>
2019-11-23 22:57:20 -0600

    Solved permutation checker and added print statement.

--- CheckForPermutation.java
+++ CheckForPermutation.java
@@ -5,27 +5,28 @@ class CheckForPermutation {
     public static boolean checkForPerms(String stringOne, String stringTwo) {
         Map<Character, Integer> map = new HashMap<>();
         
-        
         for (int i = 0; i < stringOne.length(); i ++){
             map.put(stringOne.charAt(i), map.getOrDefault(stringOne.charAt(i), 0) + 1);
-            System.out.println(stringOne.charAt(i));
         }
 
         for (int j = 0; j < stringTwo.length(); j++){
             if (map.containsKey(stringTwo.charAt(j))){
                 map.put(stringTwo.charAt(j), map.get(stringTwo.charAt(j)) - 1);
-                System.out.println(stringTwo.charAt(j));
             }
         }
 
-        return false;
-        
+        for (char key : map.keySet()){
+            if (map.get(key) != 0){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        String firstStr = "CAB";
+        String firstStr = "CABa";
         String secondStr = "ABC";
-        checkForPerms(firstStr, secondStr);
-
+        Boolean result = checkForPerms(firstStr, secondStr);
+        System.out.println(result.toString());
     }
 }
